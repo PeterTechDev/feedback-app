6a1405ab28a6e95f8fc108ea0af14e493ca94c5c
Peter <peterleite.dev@gmail.com>
2022-05-09 21:25:46 -0300

    examples of tailwind css styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,14 @@
 interface ButtonProps {
-  text: string;
+  tex?: string;
 }
 
 function Button(props: ButtonProps) {
-  return <button className="bg-violet-500 p-3">{props.text}</button>;
+  return <button className="bg-violet-500 p-3 h-10 rounded text-violet-100 hover:bg-green-400 transition-colors ">{props.text ?? 'Default'}</button>;
 }
 
 function App() {
   return (
-    <div>
+    <div className="flex gap-2">
       <Button text="eita" />
       <Button text="olha" />
       <Button text="clique" />
