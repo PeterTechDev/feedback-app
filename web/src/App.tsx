29cf1096eeefb6397fec7238a483d937ddcb7e5d
Peter <peterleite.dev@gmail.com>
2022-05-09 21:32:01 -0300

    initiang the feedback widget

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,5 @@
-interface ButtonProps {
-  tex?: string;
-}
+import { Widget } from "./components/Widget";
 
-function Button(props: ButtonProps) {
-  return <button className="bg-violet-500 p-3 h-10 rounded text-violet-100 hover:bg-green-400 transition-colors ">{props.text ?? 'Default'}</button>;
+export function App(){
+  return <Widget />
 }
-
-function App() {
-  return (
-    <div className="flex gap-2">
-      <Button text="eita" />
-      <Button text="olha" />
-      <Button text="clique" />
-    </div>
-  );
-}
-
-export default App;
