8a2461cd490e3b98564c727137c5bbd2913897c5
Peter <peterleite.dev@gmail.com>
2022-05-11 00:50:47 -0300

    add botão de fechar no form

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,7 +1,20 @@
+import { CloseButton } from './CloseButton';
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      Hello World
+      <header className="">
+
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+        <CloseButton />
+
+      </header>
+
+      <h1>Hello Peter</h1>
+
+      <footer className="text-xs ">
+        Desenvolvido por <a className="underline underline-offset-2" href="http://google.com" target="_blank">Peter Tech Dev</a>
+      </footer>
     </div>
   )
 }
